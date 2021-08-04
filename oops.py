7e31a0362c61d482fad216f0389bf74680db18b9
sumit <ben.sumit@gmail.com>
2021-08-04 16:29:27 +0530

    new line added

--- oops.py
+++ oops.py
@@ -1,1 +1,7 @@
-## this is the new file for python basics
+## this is the new file for python basic
+class sumit():
+    def __init__(self,name):
+        self.name=name
+        print(name)
+
+s=sumit("kumar")
