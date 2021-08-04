a9bf3731ad52d50e029f3a575c2dcc790d9e003a
sumit kumar <46604053+beasumit@users.noreply.github.com>
2021-08-04 16:43:01 +0530

    new code added from remote

--- oops.py
+++ oops.py
@@ -3,5 +3,8 @@ class sumit():
     def __init__(self,name):
         self.name=name
         print(name)
+    def smart(self):
+        print("sumit is doing good")
+s1=s.smart()
 
 s=sumit("kumar")
