ec3bc772ca96fd9089ceb771c8b8d488f4638a60
sumit <ben.sumit@gmail.com>
2021-08-07 09:08:50 +0530

    practisong in the morning

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 This is the File for basic pyhton dev.
 checked the value in vs code.
 checked back in web to see changes and its working for now.
+The new code is same as old one.
--- oops.py
+++ oops.py
@@ -1,12 +1,7 @@
 ## this is the new file for python basic
-class sumit():
+class sumit:
     def __init__(self,name):
         self.name=name
         print(name)
-    def now(self):
-        print("sumit is doing good")
-
-s1=now()
-print(type(s1))
-
-s2=sumit("kumar")
+s=sumit("kumar")
+print(s)
