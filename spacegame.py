49b32b0892a1b58b800fcdff1b1b22cfb1a718d9
sumit <ben.sumit@gmail.com>
2021-08-23 20:09:57 +0530

    update 08:09 pm

--- spacegame.py
+++ spacegame.py
@@ -3,12 +3,18 @@ import os
 import time
 import random
 
-#lodaing images
-RED_SPACE_SHIP = pygame.image.load(os.path.join("asset","pixel_ship_red_small.png"))
+#setting the windows for the game.
+WIDTH,HEIGHT=750,750
+WIN = pygame.display.set_mode((WIDTH,HEIGHT))
+pygame.display.set_caption("space blast")
+
+ #lodaing images
+RED_SPACE_SHIP =pygame.image.load(os.path.join("asset","pixel_ship_red_small.png"))
 BLUE_SPACE_SHIP =pygame.image.load(os.path.join("asset","pixel_ship_blue_small.png"))
 GREEN_SPACE_SHIP =pygame.image.load(os.path.join("asset","pixel_ship_green_small.png"))
+
 #player ship
-YELLOW_SPACE_SHIP =pygame.image,load(os.path.join("asset","pixel_ship_yellow.png"))
+YELLOW_SPACE_SHIP =pygame.image.load(os.path.join("asset","pixel_ship_yellow.png"))
 
 #laser/bullets
 RED_LASER =pygame.image.load(os.path.join("asset","pixel_laser_red.png"))
@@ -18,3 +24,24 @@ YELLOW_LASER =pygame.image.load(os.path.join("asset","pixel_laser_yellow.png"))
 
 #Background
 BG = pygame.image.load(os.path.join("asset","background-black.png"))
+
+#core game mechanics.
+def main():
+    run = True
+    FPS = 60
+    clock = pygame.time.Clock()
+
+    def redraw_window():
+        WIN=blit(BG,(0,0,))
+        pygame.display.updtae()
+
+    while run:
+        clock.tick(FPS)
+        redraw_window()
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                run = False     
+main()
+
+
+
