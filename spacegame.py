c6fe818e4757743fc1a4d8ae89bc2be6ad01ea55
sumit <ben.sumit@gmail.com>
2021-08-25 13:19:38 +0530

    code imporved and bug fixed

--- spacegame.py
+++ spacegame.py
@@ -4,26 +4,26 @@ import time
 import random
 
 #setting the windows for the game.
-WIDTH,HEIGHT=750,750 # we can directly use the height and width in the below bracket without making any varibale for height and width. 
+WIDTH,HEIGHT=800,600 # we can directly use the height and width in the below bracket without making any varibale for height and width. 
 WIN = pygame.display.set_mode((WIDTH,HEIGHT))
 pygame.display.set_caption("space blast")
 
  #lodaing images
-RED_SPACE_SHIP =pygame.image.load(os.path.join("asset","pixel_ship_red_small.png"))
-BLUE_SPACE_SHIP =pygame.image.load(os.path.join("asset","pixel_ship_blue_small.png"))
-GREEN_SPACE_SHIP =pygame.image.load(os.path.join("asset","pixel_ship_green_small.png"))
+RED_SPACE_SHIP =pygame.image.load(os.path.join("assets","pixel_ship_red_small.png"))
+BLUE_SPACE_SHIP =pygame.image.load(os.path.join("assets","pixel_ship_blue_small.png"))
+GREEN_SPACE_SHIP =pygame.image.load(os.path.join("assets","pixel_ship_green_small.png"))
 
 #player ship
-YELLOW_SPACE_SHIP =pygame.image.load(os.path.join("asset","pixel_ship_yellow.png"))
+YELLOW_SPACE_SHIP =pygame.image.load(os.path.join("assets","pixel_ship_yellow.png"))
 
 #laser/bullets
-RED_LASER =pygame.image.load(os.path.join("asset","pixel_laser_red.png"))
-BLUE_LASER =pygame.image.load(os.path.join("asset","pixel_laser_blue.png"))
-GREEN_LASER =pygame.image.load(os.path.join("asset","pixel_laser_green.png"))
-YELLOW_LASER =pygame.image.load(os.path.join("asset","pixel_laser_yellow.png"))
+RED_LASER =pygame.image.load(os.path.join("assets","pixel_laser_red.png"))
+BLUE_LASER =pygame.image.load(os.path.join("assets","pixel_laser_blue.png"))
+GREEN_LASER =pygame.image.load(os.path.join("assets","pixel_laser_green.png"))
+YELLOW_LASER =pygame.image.load(os.path.join("assets","pixel_laser_yellow.png"))
 
 #Background
-BG = pygame.image.load(os.path.join("asset","background-black.png"))
+BG = pygame.image.load(os.path.join("assets","background-black.png"))
 
 #core game mechanics.
 def main():
@@ -33,14 +33,14 @@ def main():
 
     def redraw_window():
         WIN.blit(BG, (0, 0))
-        pygame.display.updae()
+        pygame.display.update()
 
     while run:
         clock.tick(FPS)
         redraw_window()
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
-                run = False     
+                run = False 
 main()
 
 
